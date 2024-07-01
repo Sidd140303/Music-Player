569fa3c6d6a0ca95cb3d61fc8d076330194d40ad
Siddharth Gupta <siddharth140303@gmail.com>
2024-07-02 01:24:16 +0530

    Adding Files

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { spotify23 } from './services/shazamCore';
+import playerReducer from './features/playerSlice';
+
+export const store = configureStore({
+  reducer: {
+    [spotify23.reducerPath]: spotify23.reducer,
+    player: playerReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(spotify23.middleware)
+});
